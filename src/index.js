f0fdeb836876d58973b420dd0de0f4b552b05c4d
jacek_roszko <jacek@Y50.localdomain>
2021-01-22 18:01:17 +0100

    product list rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const auth = require("./api/auth");
 const store = require("./api/store");
 
 const app = express();
 const port = 3004;
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use(auth);
 app.use(store);
